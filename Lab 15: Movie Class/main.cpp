302b028709c98545e62a90c50d91846377c16e04
Guo An Wang <you@example.com>
2025-09-27 22:52:29 -0700

    Initial commit

---
+++ Lab 15: Movie Class/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMSC-210 | Lab 14: Movie Class | Guo An Wang
+//  IDE used: Xcode
+//  Created by Guo An Wang on 9/27/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
