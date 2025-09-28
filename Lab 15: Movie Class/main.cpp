c393eb3f32d6ad830d0d42bb4218cade4bf5dd6a
Guo An Wang <you@example.com>
2025-09-27 23:03:01 -0700

    second commit

--- Lab 15: Movie Class/main.cpp
+++ Lab 15: Movie Class/main.cpp
@@ -6,10 +6,37 @@
 //
 
 #include <iostream>
+#include <iomanip>
+#include <vector>
 using namespace std;
 
+int WIDTH = 10;
+
+class Movie
+{
+private:
+    string name;
+    int year;
+    string screenWriter;
+public:
+    //getter functions
+    string getName() {return name;}
+    int getYear() {return year;}
+    string getScreenWriter() {return screenWriter;}
+    //setter functions
+    string setName(string n) {name = n; return n;}
+    int setYear(int y) {year = y; return y;}
+    string setScreenWriter(string s) {screenWriter = s; return s;}
+    
+    void print(){
+        cout << "Movie: " << name << endl;
+        cout << setw(WIDTH) << "Year: " << year << endl;
+        cout << setw(WIDTH) << "Screen Writer: " << screenWriter << endl;
+    }
+    
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    vector <Movie> movie;
+    ifstream fin("Movies.txt");
 }
